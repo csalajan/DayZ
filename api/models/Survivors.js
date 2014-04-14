e581da1b2c2c1eca157f93afc2d998a057f9ccff
Craig <craig@mint-test.com>
2014-04-14 11:20:07 -0400

    Survivors Controller

--- api/models/Survivors.js
+++ api/models/Survivors.js
@@ -2,9 +2,11 @@ var Survivors = {
 	attributes: {
 		name: {
 			type: 'STRING',
+			defaultsTo: '',
 		},
 		location: {
 			type: 'STRING',
+			defaultsTo: '',
 		},
 		blood: {
 			type: 'STRING',
